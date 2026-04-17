39fc4d72a8853ced76c706369613de1dd659efda
Laura Buaillon <laura.buaillon1@gmail.com>
2026-04-17 11:37:40 +0200

    page register: resolution problème affiche mot de passe

--- assets/controllers/password_toggle.js
+++ assets/controllers/password_toggle.js
@@ -1,11 +1,14 @@
-document.querySelectorAll(".eye-hide").forEach(function (icon) {
-    icon.addEventListener("click", function () {
-        const input = document.getElementById(this.dataset.target);
-        //si le type est password on le change en text (mdp visible),sinon on remet password
-        if (input.type === "password") {
-            input.type = "text";
-        } else {
-            input.type = "password";
-        }
+document.addEventListener("DOMContentLoaded", function () {
+    document.querySelectorAll(".auth-card__eye").forEach(function (icon) {
+        icon.addEventListener("click", function () {
+            const input = document.getElementById(this.dataset.target);
+            //si le type est password on le change en text (mdp visible),sinon on remet password
+             if (!input) return;
+            if (input.type === "password") {
+                input.type = "text";
+            } else {
+                input.type = "password";
+            }
+        });
     });
 });
